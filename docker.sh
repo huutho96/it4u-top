fde785783dc4c819ee401bcfb87d0a584389c4a5
Louis Nguyen <thonh.it@gmail.com>
2021-01-20 11:12:10 +0700

    update script

--- docker.sh
+++ docker.sh
@@ -2,11 +2,9 @@
 version=0.0.1
 
 # build source code
-cd source 
 JEKYLL_ENV=production bundle exec jekyll build
 
 # build docker image and push to docker hub
-cd ../
 docker build -t it4u-portal:$version .
 docker tag it4u-portal:$version louisnguyen96/it4u-portal:$version
 docker push louisnguyen96/it4u-portal:$version
